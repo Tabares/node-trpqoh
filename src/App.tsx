a124bfb099673f2f5f5b1f8c4ef3d52cbd2635e6
Tabares Sotelo <jose.tabares.sotelo@accenture.com>
2022-06-22 16:27:31 -0500

    Add initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import * as React from 'react';
-import './style.css';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-export default function App() {
+function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
+
+export default App;
